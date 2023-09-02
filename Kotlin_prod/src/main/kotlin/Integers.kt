7bf7a8a2b740c9bce366224a7918bc49d5a5fd09
Nazar Mik <nazarmik98@gmail.com>
2023-09-02 08:17:02 +0300

    added new class with integers

---
+++ Kotlin_prod/src/main/kotlin/Integers.kt
@@ -0,0 +1,17 @@
+fun main() {
+        val size: Byte = 15
+        val age: Short = 12
+        val id: Int = 1337
+        val prize: Long = 1_000_000_000
+
+
+        val fiveAsByte: Byte = 5
+        val fiveAsShort: Short = 5
+        val fiveAsLong: Long = 5
+
+        val qwerty: Byte = 127
+
+        val myResult = size + age.toDouble()
+        println(myResult)
+    }
+
---
+++ Kotlin_prod/src/main/kotlin/MyClass.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Hello boy!")
+}
+
+var age: Int = 5;
+
+
+
