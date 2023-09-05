a2ce8b9baa94ae7c5033478e307312671c37bf4a
Nazar Mik <nazarmik98@gmail.com>
2023-09-05 12:21:10 +0300

    create new class IfExample

---
+++ Kotlin_prod/src/main/kotlin/IfExample.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+   val name: String = "Alex"
+   val age: Int = 30
+   val isProgrammer: Boolean = false
+   val isUberDriver: Boolean = false
+    val isTeacher: Boolean = true
+
+    println("Hello my name is $name")
+
+    if (isProgrammer){
+        println("I am a programmer!")
+    }else{
+        println("I have not a job...")
+    }
+
+    println("that's all!!!")
+}
