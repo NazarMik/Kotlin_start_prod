a3b199dabb76e74819c0b1ba6faeac03d9dde8ac
Nazar Mik <nazarmik98@gmail.com>
2023-09-07 15:26:09 +0300

    create class WhenClass

---
+++ Kotlin_prod/src/main/kotlin/WhenClass.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val x: Int = 3
+
+    when (x){
+       1 -> { println("1") }
+       2 -> { println("2") }
+       3 -> { println("3") }
+    }
+
+    val age: Int = 20
+    when (age >= 18){
+        false -> { println("1") }
+        true -> { println("2") }
+    }
+
+    
+}
