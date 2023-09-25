b905607a162b984a6c3476c610d4c115fb70c4c4
Nazar Mik <nazarmik98@gmail.com>
2023-09-25 10:34:53 +0300

    create full inner class

--- Kotlin_prod/src/main/kotlin/InnerClass.kt
+++ Kotlin_prod/src/main/kotlin/InnerClass.kt
@@ -1,9 +1,24 @@
 class InnerClass {
-    
+
     var brand = "BMW"
 
     inner class Inner{
         fun run() = true
         fun stop() = false
+    }
+}
+
+class Smartphone{
+    val battery = 4000
+
+    inner class  GooglePixel{
+        fun about() = "Pixel has $battery mAh"
+    }
+
+    fun  main(){
+        Smartphone().battery
+        Smartphone().GooglePixel()
+        Smartphone().GooglePixel().about()
+        
     }
 }
