bcbc91313de306143d990e10e35a4fc9628707a6
Nazar Mik <nazarmik98@gmail.com>
2023-09-26 07:55:34 +0300

    write a code in the class Object

--- Kotlin_prod/src/main/kotlin/ClassObject.kt
+++ Kotlin_prod/src/main/kotlin/ClassObject.kt
@@ -1,2 +1,9 @@
-class ClassObject {
+fun main() {
+val helloWorld = object {
+    val  hello = "hello"
+    val world = "world"
+
+    override fun toString() = "$hello $world"
+}
+    println(helloWorld.toString())
 }
